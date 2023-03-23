f14342a6a438c838acd00b41225159d711b7c5f6
CristianUva <cristian.uva@studenti.isissgobetti.it>
2023-03-23 13:54:09 +0100

    Finito es 5

--- es_date.py
+++ es_date.py
@@ -2,8 +2,8 @@
 #Importo la libreria
 import datetime
 #Esercizio 1 Stampo data corrente
-x = datetime.datetime.now()
-print(x)
+y = datetime.datetime.now()
+print(y)
 #Esercizio 2 Creo un adata con il mio compleanno
 x = datetime.datetime(2006, 2, 9)
 #Esercizio 3 Stampo la data del mio compleanno numerico
@@ -11,3 +11,5 @@ print(x)
 #Esercizio 4 Stampo la data del vostro compleanno il più possibile come stringa: Wednesay 31 December 1975
 print(x.strftime("%A %d %B %Y "))
 #Esercizio 5 Calcolo la differenza tra la data corrente e la data del vostro compleanno
+differenza = y - x
+print(f"{differenza}")
